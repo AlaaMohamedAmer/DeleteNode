5c03c2b33ed3f5c51c5d33c24d020ef4b6c89514
alaa <aamal79545@gmail.com>
2024-02-09 20:45:29 +0200

    Add project files.

---
+++ DeleteNode/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteNode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public class Solution
+    {
+        public void DeleteNode(ListNode node)
+        {
+            while (node != null)
+            {
+                node.val = node.next.val;
+                if (node.next.next == null)
+                {
+                    node.next = null;
+                }
+                node = node.next;
+            }
+
+        }
+    }
+}
